8879c9396e377824ccc466b83099dba252b9cd8f
UlanbekRasul <ulanbekrasul@gmail.com>
2021-12-09 20:55:58 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lesson31/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.lesson31;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image=findViewById(R.id.image);
+    }
+}
